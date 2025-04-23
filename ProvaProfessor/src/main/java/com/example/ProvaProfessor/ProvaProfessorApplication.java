17849e82d445e4878ce3c7f3b8bf55dea58fd578
Felipe Martins <felipemrtns6002199@gmail.com>
2025-04-23 13:39:53 -0300

    Add files via upload

---
+++ ProvaProfessor/src/main/java/com/example/ProvaProfessor/ProvaProfessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProvaProfessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaProfessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaProfessorApplication.class, args);
+	}
+
+}
